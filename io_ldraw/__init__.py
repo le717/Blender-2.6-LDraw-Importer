b5c0657c59413ee9d7ca0e865d32c909d6870d36
Banbury <banbury20@gmail.com>
2014-04-13 15:10:11 +0200

    Added simple exporter.

--- io_ldraw/__init__.py
+++ io_ldraw/__init__.py
@@ -46,7 +46,6 @@ class LDRImporterOps(bpy.types.Operator, ImportHelper):
         # pr = cProfile.Profile()
         # pr.enable()
 
-        import os
         parser = LDrawParser(os.path.dirname(self.filepath))
         parser.basepath = self.ldraw_path
         parser.scale = self.scale / 100
@@ -62,12 +61,43 @@ class LDRImporterOps(bpy.types.Operator, ImportHelper):
 
         return {'FINISHED'}
 
+class LDRExporterOps(bpy.types.Operator, ImportHelper):
+    """LDR Importer Operator"""
+    bl_idname = "export_scene.ldraw"
+    bl_description = "Export an LDraw model (.ldr/.dat)"
+    bl_label = "Export LDraw Model"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_options = {'REGISTER', 'UNDO', 'PRESET'}
+
+    # File type filter in file browser
+    filename_ext = ".ldr"
+
+    filter_glob = bpy.props.StringProperty(
+        default="*.ldr;*.dat",
+        options={'HIDDEN'}
+    )
+
+    ldraw_path = bpy.props.StringProperty(name="LDraw path", default="c:\\ldraw" if sys.platform.startswith("win") else os.path.join(os.path.expanduser("~"), "ldraw"))
+    scale = bpy.props.FloatProperty(name="Scale", default=100, min=0)
+
+    def execute(self, context):
+        from io_ldraw.ldrawexport import LDrawExporter
+        exporter = LDrawExporter(self.filepath, self.scale)
+        exporter.export()
+
+        return {'FINISHED'}
+
 def menu_import(self, context):
     """Import menu listing label"""
     self.layout.operator(LDRImporterOps.bl_idname, text="LDraw (.ldr/.dat)")
 
     import io_ldraw.materials
     io_ldraw.materials.is_cycles = context.scene.render.engine == 'CYCLES'
+
+def menu_export(self, context):
+    """Export menu listing label"""
+    self.layout.operator(LDRExporterOps.bl_idname, text="LDraw (.ldr/.dat)")
 
 def register():
     """Register Menu Listing"""
@@ -76,13 +106,14 @@ def register():
 
     bpy.utils.register_module(__package__)
     bpy.types.INFO_MT_file_import.append(menu_import)
+    bpy.types.INFO_MT_file_export.append(menu_export)
 
 
 def unregister():
     """Unregister Menu Listing"""
     bpy.utils.unregister_module(__package__)
     bpy.types.INFO_MT_file_import.remove(menu_import)
-
+    bpy.types.INFO_MT_file_export.remove(menu_export)
 
 if __name__ == "__main__":
     register()
---
+++ io_ldraw/ldrawexport.py
@@ -0,0 +1,45 @@
+import bpy
+import os
+
+class LDrawExporter:
+    def __init__(self, modelpath, scale):
+        self.path = modelpath
+        self.scale = scale
+
+    def export(self):
+        if bpy.context.active_object is None or bpy.context.active_object.type != 'MESH' or bpy.context.active_object not in bpy.context.selected_objects:
+            print("No model selected.")
+
+        self.file = open(self.path, "w")
+
+        import bmesh
+        bm = bmesh.new()
+        bm.from_mesh(bpy.context.active_object.data)
+
+        for f in bm.faces:
+            if len(f.verts) == 3:
+                self.write_triangle(f)
+            elif len(f.verts) == 4:
+                self.write_quad(f)
+            else:
+                print("Too many vertices.")
+
+        bm.free()
+
+        self.file.close()
+
+    def write_triangle(self, face):
+        self.file.write("3 16")
+        self.write_verts(face)
+        self.file.write("\n")
+
+    def write_quad(self, face):
+        self.file.write("4 16")
+        self.write_verts(face)
+        self.file.write("\n")
+
+    def write_verts(self, face):
+        for vert in face.verts:
+            v = vert.co
+            self.file.write(" {0} {1} {2}".format(round(v.x, 5) * self.scale, round(v.y, 5) * self.scale, round(v.z, 5) * self.scale))
+
